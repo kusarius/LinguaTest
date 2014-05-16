e8f0700fb4c0d439ab41bd170967fe00e2743143
Alexander Ragulin <kusarius@gmail.com>
2014-05-16 16:45:18 +0300

    0003

--- LinguaTest/MainWindow.Designer.cs
+++ LinguaTest/MainWindow.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tESTToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.hELPToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startLabel = new System.Windows.Forms.Label();
+            this.создатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.открытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеLinguaTestToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оставитьОтзывToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tESTToolStripMenuItem,
+            this.hELPToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(762, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tESTToolStripMenuItem
+            // 
+            this.tESTToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.создатьToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.открытьToolStripMenuItem,
+            this.сохранитьToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.выходToolStripMenuItem});
+            this.tESTToolStripMenuItem.Name = "tESTToolStripMenuItem";
+            this.tESTToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.tESTToolStripMenuItem.Text = "ТЕСТ";
+            // 
+            // hELPToolStripMenuItem
+            // 
+            this.hELPToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.оПрограммеLinguaTestToolStripMenuItem,
+            this.оставитьОтзывToolStripMenuItem});
+            this.hELPToolStripMenuItem.Name = "hELPToolStripMenuItem";
+            this.hELPToolStripMenuItem.Size = new System.Drawing.Size(73, 20);
+            this.hELPToolStripMenuItem.Text = "СПРАВКА";
+            // 
+            // startLabel
+            // 
+            this.startLabel.AutoSize = true;
+            this.startLabel.Font = new System.Drawing.Font("Segoe UI Light", 12F);
+            this.startLabel.Location = new System.Drawing.Point(116, 225);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(510, 21);
+            this.startLabel.TabIndex = 1;
+            this.startLabel.Text = "Для начала работы создайте новый тест или откройте существующий...";
+            // 
+            // создатьToolStripMenuItem
+            // 
+            this.создатьToolStripMenuItem.Name = "создатьToolStripMenuItem";
+            this.создатьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.создатьToolStripMenuItem.Text = "Создать";
+            this.создатьToolStripMenuItem.Click += new System.EventHandler(this.создатьToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(149, 6);
+            // 
+            // открытьToolStripMenuItem
+            // 
+            this.открытьToolStripMenuItem.Name = "открытьToolStripMenuItem";
+            this.открытьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.открытьToolStripMenuItem.Text = "Открыть";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(149, 6);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // оПрограммеLinguaTestToolStripMenuItem
+            // 
+            this.оПрограммеLinguaTestToolStripMenuItem.Name = "оПрограммеLinguaTestToolStripMenuItem";
+            this.оПрограммеLinguaTestToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.оПрограммеLinguaTestToolStripMenuItem.Text = "О программе LinguaTest";
+            // 
+            // оставитьОтзывToolStripMenuItem
+            // 
+            this.оставитьОтзывToolStripMenuItem.Name = "оставитьОтзывToolStripMenuItem";
+            this.оставитьОтзывToolStripMenuItem.Size = new System.Drawing.Size(210, 22);
+            this.оставитьОтзывToolStripMenuItem.Text = "Оставить отзыв";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(762, 472);
+            this.Controls.Add(this.startLabel);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "MainWindow";
+            this.Text = "LinguaTest";
+            this.Resize += new System.EventHandler(this.MainWindow_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tESTToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem hELPToolStripMenuItem;
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.ToolStripMenuItem создатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem открытьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеLinguaTestToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оставитьОтзывToolStripMenuItem;
     }
 }
 
--- LinguaTest/MainWindow.cs
+++ LinguaTest/MainWindow.cs
@@ -2,6 +2,16 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+/* 
+ * LinguaTest main window.
+ * 
+ * -----------------------------------------------
+ * 
+ * Author: Alexander Ragulin (kusarius@gmail.com)
+ * 16.05.2014
+ *
+*/
+
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -14,6 +24,27 @@ namespace LinguaTest
         public MainWindow()
         {
             InitializeComponent();
+
+            // Locate startLabel to the center of the screen
+            startLabel.Location = new Point(this.Width / 2 - startLabel.Width / 2,
+                this.Height / 2 - startLabel.Height / 2 - 20);
+        }
+
+        private void MainWindow_Resize(object sender, EventArgs e)
+        {
+            if (startLabel.Visible == true) 
+                startLabel.Location = new Point(this.Width / 2 - startLabel.Width / 2,
+                    this.Height / 2 - startLabel.Height / 2 - 20);
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void создатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
