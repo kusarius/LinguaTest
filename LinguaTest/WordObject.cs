d074d84719d36aa04b7a623c34faa7cc8499ddad
Alexander Ragulin <kusarius@gmail.com>
2014-05-16 18:16:11 +0300

    0006

---
+++ LinguaTest/WordObject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinguaTest
+{
+    class WordObject
+    {
+        public enum PartOfSpeechKind
+        {
+            Noun, Verb, Adjective,
+            Article, Pronoun, Numeral,
+            Adverb, Preposition,
+            Conjuction
+        }
+
+        public string Word { get; set; }
+        public string Translate { get; set; }
+        public PartOfSpeechKind PartOfSpeech { get; set; }
+
+        public WordObject(string w, string t, PartOfSpeechKind pof)
+        {
+            Word = w;
+            Translate = t;
+            PartOfSpeech = pof;
+        }
+        public WordObject() { }
+    }
+}
