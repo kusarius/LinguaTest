5b7076a84ac614cec4e20e5924dda585c8921cb1
Alexander Ragulin <kusarius@gmail.com>
2014-05-17 10:10:40 +0300

    0009

---
+++ LinguaTest/CreateOrEditTestWindow.cs
@@ -0,0 +1,100 @@
+﻿/* 
+ * A window for creating or editing tests.
+ * 
+ * -----------------------------------------------
+ * 
+ * Author: Alexander Ragulin (kusarius@gmail.com)
+ * 16.05.2014
+ *
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LinguaTest
+{
+    public partial class CreateOrEditTestWindow : Form
+    {
+        private bool isSave = true;
+
+        public CreateOrEditTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Add word button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WordObject res = AddNewWordWindow.ShowWindowDialog("", "", "", "Добавить слово");
+            if (res.Translate != null)
+            {
+                dataGridView1.Rows.Add(res.Word, res.Translate, WordObject.GetPartOfSpeechString(res.PartOfSpeech));
+                isSave = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                dataGridView1.Rows.Remove(dataGridView1.SelectedRows[0]);
+                isSave = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow r = dataGridView1.SelectedRows[0];
+                WordObject res = AddNewWordWindow.ShowWindowDialog(r.Cells[0].Value.ToString(),
+                    r.Cells[1].Value.ToString(), r.Cells[2].Value.ToString(), "Редактировать слово");
+                if (res.Translate != null)
+                {
+                    dataGridView1.SelectedRows[0].Cells[0].Value = res.Word;
+                    dataGridView1.SelectedRows[0].Cells[1].Value = res.Translate;
+                    dataGridView1.SelectedRows[0].Cells[2].Value = WordObject.GetPartOfSpeechString(res.PartOfSpeech);
+                    isSave = false;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.Rows.Count > 0)
+            {
+                SaveFileDialog d = new SaveFileDialog();
+                d.Filter = "Файлы тестов LinguaTest (*.ltt)|*.ltt";
+
+                if (d.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    string output = "";
+                    foreach (DataGridViewRow i in dataGridView1.Rows)
+                    {
+                        output += i.Cells[0].Value.ToString() + ":::";
+                        output += i.Cells[1].Value.ToString() + ":::";
+                        output += i.Cells[2].Value.ToString() + Environment.NewLine;
+                    }
+                    File.WriteAllText(d.FileName, output,Encoding.Default);
+                    isSave = true;
+                }
+            }
+        }
+
+        private void CreateOrEditTestWindow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (isSave) return;
+            DialogResult dr = MessageBox.Show("Сохранить изменения в тесте?", "LinguaTest", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if (dr == System.Windows.Forms.DialogResult.Yes) 
+                button4_Click(null,null);
+        }
+    }
+}
---
+++ LinguaTest/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LinguaTest
+{
+    class Utils
+    {
+        public static string FormalWord(string w)
+        {
+            w = w.ToLower();
+            return w[0].ToString().ToUpper() + w.Substring(1, w.Length - 1);
+        }
+    }
+}
